d23cbfdebcadaf017e91de21f82ff08e20e4b3f0
EvanderRPT <evanderrampenter@gmail.com>
2025-04-22 08:33:31 +0800

    add delay

--- raycasting-c/src/main.c
+++ raycasting-c/src/main.c
@@ -5,7 +5,7 @@
 SDL_Window* window = NULL;
 SDL_Renderer* renderer = NULL;
 int isGameRunning = FALSE;
-
+int ticksLastFrame;
 int playerX, playerY;
 
 int initializeWindow() {
@@ -63,8 +63,15 @@ void processInput() {
 }
 
 void update() {
-    playerX += 1;
-    playerY += 1;
+
+    while (!SDL_TICKS_PASSED(SDL_GetTicks(), ticksLastFrame + FRAME_TIME_LENGTH));
+
+    float deltaTime = (SDL_GetTicks() - ticksLastFrame) / 1000.0f;
+
+    ticksLastFrame = SDL_GetTicks();
+
+    playerX += 50 * deltaTime;
+    playerY += 50 * deltaTime;
 }
 
 void render() {
